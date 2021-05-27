86c9eaa0ffd85dbd98a8d0bb2a06b92204647a97
Bradley Archer <bradley.archer@paulsmith.co.uk>
2021-05-27 23:16:08 +0100

    Fix colour inversion, light/dark mode

--- js/light-dark-mode.js
+++ js/light-dark-mode.js
@@ -1,14 +1,5 @@
 $(document).ready(function() {
 	$('.switch input').change(function() {
-		console.log('hit');
-		var body = this.checked ? 'white' : '#2e2e2e',
-			bodyFont = this.checked ? 'black' : 'white',
-			nav  = this.checked ? 'white' : '#1c1c1c',
-			header = this.checked ? 'white' : '#1c1c1c',
-			footer = this.checked ? 'white' : '#1c1c1c';
-
-		$('body').css('background-color', body);
-		$('header').css('background-color', header);
-		$('footer').css('background-color', footer);
+		$('html').toggleClass("inversed");
 	});
 });

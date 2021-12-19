e12d4ab8482c63822fe943729d8f9bdd7a1d2f17
Brad Archer <bradarcher15@googlemail.com>
2021-12-19 08:47:57 -0800

    Added new colours, added terminal themed sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,13 @@ module.exports = {
         'dark-1' : '#3B3F46',
         'dark-2' : '#2A2E34',
         'dark-3' : '#1E2328',
+        'close': '#EC6A5F',
+        'close-inner': '#A90000',
+        'minimize': '#F5C04F',
+        'maximize': '#64CC57',
+        'close-dark': '#D04E42',
+        'minimize-dark': '#D6A13D',
+        'maximize-dark': '#4EA73B'
       }
     },
     screens: {

3bd5ccf14d9c28bc87154d6354fc63047ea903ed
Brad Archer <bradarcher15@googlemail.com>
2023-06-24 14:08:44 -0700

    Dark Mode Memory & Icon Change

--- js/light-dark-mode.js
+++ js/light-dark-mode.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
-	$('.switch input').change(function() {
+	let darkModeValue = localStorage.getItem("dark");
+
+	// Dark mode is active.
+	if (darkModeValue === '1') {
 		$('html').toggleClass("inversed");
+
+		$('.dark-mode-icon').toggleClass("fa-moon");
+		$('.dark-mode-icon').toggleClass("fa-sun");
+		$('.switch input').prop('checked', true);
+	}
+
+	$('.switch input').change(function() {
+		if (+window.localStorage.getItem("dark")) {
+			window.localStorage.setItem("dark", 0);
+		} else {
+		    window.localStorage.setItem("dark", 1);
+		}
+		$('html').toggleClass("inversed");
+		$('.dark-mode-icon').toggleClass("fa-moon");
+		$('.dark-mode-icon').toggleClass("fa-sun");
 	});
 });

51f560d6b69fcd740a3b2f0c137fcf6266880b31
Brad Archer <bradarcher15@googlemail.com>
2023-06-24 14:36:47 -0700

    Fix Placement of Errors on Contact Form

--- js/form-validation.js
+++ js/form-validation.js
@@ -2,5 +2,8 @@ $(document).ready(function() {
 	let form = $('.contact-form');
 
 	form.validate({
+		errorPlacement: function(error, element) {
+           error.insertAfter(element);
+       },
 	});
 });
